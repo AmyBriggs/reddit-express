a35ed5f22351fcc84cdd0441d9cb6858669e8661
Amy Briggs <amylbd@gmail.com>
2016-11-06 16:37:28 -0700

    getting some posts up on the page

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,7 +4,57 @@ var knex = require('../db/knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('posts')
+    .then(function(data) {
+      res.json(data)
+    })
 });
+
+router.post('/', function(req, res, next) {
+  knex('posts')
+    .insert(req.body, '*')
+    .then(function(post) {
+      res.json(post)
+    })
+})
+
+router.get('/:id', function(req, res, next){
+  knex('posts')
+  .where('id', req.params.id)
+  .first()
+  .then(function(post) {
+    res.json(post)
+  })
+})
+
+router.put('/:id', function(req, res, next){
+  knex('posts')
+  .where('id', req.params.id)
+  .first()
+  .then(function(post){
+    if(post.users_id === req.body.users_id) {
+      knex('posts')
+      .where('id', req.params.id)
+      .update(req.body)
+      .then(function(post){
+        res.json(post)
+      })
+    } else {
+      let error = {
+        message: 'Please log in.'
+      }
+    } res.json(error)
+  })
+})
+
+router.delete('/:id', function(req, res, next){
+  knex('posts')
+    .where('id', req.params.id)
+    .first()
+    .del()
+    .then(function(){
+      res.json('Post deleted')
+    })
+})
 
 module.exports = router;
