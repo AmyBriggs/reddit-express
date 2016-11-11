3d1d8a243d6235bd3f9d524750852fd6260aec53
Amy Briggs <amylbd@gmail.com>
2016-11-11 06:30:44 -0700

    still fixing bugs in edit comment

--- client/javascripts/controllers/edit_comment_controller.js
+++ client/javascripts/controllers/edit_comment_controller.js
@@ -5,19 +5,25 @@ app.controller('EditCommentController', function($scope, PostService, $location,
   $scope.id = ''
 
   let cookie = $cookies.getObject('loggedIn')
+  console.log('loggedIn',cookie);
 
   $scope.editedComment = PostService.comments.get({id: $routeParams.id}, function() {})
+  console.log(cookie.id);
+
+
+
 
   $scope.submitEditedComment = function(comment){
     if(!cookie){
-      $scope.error = 'Please log in to edit comment.'
+      $scope.error = 'You must be the author to edit this comment.  Please log in.'
     } else if (comment.users_id === cookie.id){
+      console.log('user is', user);
       PostService.comments.update(comment, function(){
         $scope.id = comment.posts_id
         $location.url(`/${$scope.id}`)
       })
     } else {
-      $scope.error = 'Please log in to edit comment.'
+      $scope.error = 'You must be the author to edit this comment.  Please log in.'
     }
   }
 
